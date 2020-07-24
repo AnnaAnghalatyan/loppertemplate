0e06b5b1a3c61c64c06a5eed77e04ac1af29e7d7
AnnaAnghalatyan <a.anghalatyan@gmail.com>
2020-07-24 18:48:38 +0400

    menu changed

--- js/main.js
+++ js/main.js
@@ -2,32 +2,44 @@ const scrollBtn = document.getElementById('scroll-down');
 const menuToggler = document.querySelector('.header__toggler ');
 const overlay = document.querySelector('.header__menu-overlay');
 const menuLinks = [...document.querySelectorAll('.header__menu-link')];
+const navbar = document.querySelector('.header__navbar');
+let menuClosed = true;
 
 const menu = document.querySelector('.header__menu');
 
-scrollBtn.onclick = function (e) {
+if (scrollBtn) scrollBtn.onclick = function (e) {
     const h = window.innerHeight;
-    window.scrollTo(0, h);
+    const navHeight = navbar.offsetHeight;
+    window.scrollTo(0, h - navHeight);
 }
 
+window.addEventListener('scroll', function () {
+    this.scrollY > 100 ?
+        navbar.classList.add('purple') :
+        navbar.classList.remove('purple')
+
+})
+
 function openMenu() {
+    menuClosed = false;
     menu.classList.add('toggled');
     document.body.classList.add('scroll-off');
     overlay.classList.add('fade')
 }
 
 function closeMenu() {
+    menuClosed = true;
     menu.classList.remove('toggled');
     document.body.classList.remove('scroll-off');
     overlay.classList.remove('fade')
 }
 
-menuToggler.onclick = function (e) {
-    openMenu();
+if(menuToggler) menuToggler.onclick = function (e) {
+    menuClosed ? openMenu() : closeMenu();
 }
 
-overlay.onclick = () => closeMenu()
+if(overlay) overlay.onclick = () => closeMenu()
 
-menuLinks.forEach(link => link.addEventListener('click', function () {
+if(menuLinks) menuLinks.forEach(link => link.addEventListener('click', function () {
     closeMenu();
 }))
